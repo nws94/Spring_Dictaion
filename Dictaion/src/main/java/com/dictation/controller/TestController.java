dead6bc19fdd9c479da68413bd57fa96db40a394
남우석 <hinyong22@gmail.com>
2020-04-23 16:24:08 +0900

    FrontEnd와 통신하기 위한 설정

--- Dictaion/src/main/java/com/dictation/controller/TestController.java
+++ Dictaion/src/main/java/com/dictation/controller/TestController.java
@@ -3,6 +3,7 @@ package com.dictation.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import com.dictation.service.TestService;
 import com.dictation.vo.TestVO;
 
 @RestController
+@CrossOrigin("http://localhost:8080/")
 @RequestMapping(value="/test")
 public class TestController {
 	
